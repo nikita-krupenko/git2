ac121bc089531f13b64deb43da6f1f0b83717a11
nikita-krupenko <nikitoss.krupenko.github@gmail.com>
2023-10-11 18:15:20 +0300

    Initial Commit

---
+++ git2/git2App.swift
@@ -0,0 +1,17 @@
+//
+//  git2App.swift
+//  git2
+//
+//  Created by nikita on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct git2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
