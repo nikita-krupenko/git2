b89ddbdfe804024914153d0080265384101b03ef
nikita-krupenko <nikitoss.krupenko.github@gmail.com>
2023-10-11 18:22:10 +0300

    add there

--- git2/ContentView.swift
+++ git2/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("Hello, world!")
+            Text("Hello, world!")
         }
         .padding()
         .foregroundColor(.gray)
